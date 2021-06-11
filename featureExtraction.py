cb91e777a9be581ba31808408a68ea60026be042
MoRadman11 <moramdan481@gmail.com>
2021-06-11 08:49:04 +0200

    define feature extraction class and apply histogram algorithm

---
+++ featureExtraction.py
@@ -0,0 +1,80 @@
+import cv2 as cv
+import imutils
+import numpy as np
+class FeatureExtraction:
+    def __init__(self):
+        self.bins = (8, 12, 4)
+
+    def get_mean(self, image):
+        img2_av_R = np.mean(image[:, :, 2])
+        img2_av_G = np.mean(image[:, :, 1])
+        img2_av_B = np.mean(image[:, :, 0])
+        return [img2_av_R, img2_av_G, img2_av_B]
+
+    def crop_vertical(self, img):
+        width = img.shape[1]
+        widthCutOff = width // 2
+        leftImg = img[:, 0: widthCutOff]
+        rightImg = img[:, widthCutOff:]
+        return (leftImg, rightImg)
+
+    def crop_horrizonal(self, img):
+        height = img.shape[0]
+        heightCutOff = height // 2
+        upperImg = img[0: heightCutOff, :]
+        lowerImg = img[heightCutOff:, :]
+        return (upperImg, lowerImg)
+
+    def crop_img(self, img, numOfCuts):
+        (leftImg, rightImg) = self.crop_vertical(img)
+        (firstQuarter, thirdQuarter) = self.crop_horrizonal(leftImg)
+        (secondQuarter, forthQuarter) = self.crop_horrizonal(rightImg)
+        return [firstQuarter, secondQuarter, thirdQuarter, forthQuarter]
+
+    def get_color_layout(self, image):
+        image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+        features_vector = []
+        layouts = self.crop_img(image, 0)
+        for i in layouts:
+            hist = self.histogram(i, None)
+            features_vector.append(hist)
+        return features_vector
+
+    def get_histogram(self, image):
+        image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+        return self.histogram(image, None)
+
+    def histogram(self, image, mask=None):
+        histogram = cv.calcHist([image], [0, 1, 2], mask, self.bins,
+                                [0, 180, 0, 256, 0, 256])
+        if imutils.is_cv2():
+            histogram = cv.normalize(histogram).flatten()
+        else:
+            histogram = cv.normalize(histogram, histogram).flatten()
+        return histogram
+
+# descriptor = FeatureExtraction()
+# img = cv.imread('images/Salah2.jpg')
+# #img = cv.resize(img, (600, 500), interpolation=cv.INTER_AREA)
+# img2 = cv.imread('images/salah1.jpg')
+# img2 = cv.resize(img2, (600, 500), interpolation=cv.INTER_AREA)
+# v1 = descriptor.histogram(img, (8, 12, 4), None)
+# v2 = descriptor.histogram(img2, (8, 12, 4), None)
+# # print(v1)
+# # print('-------------------------')
+# # print(v2)
+def compareHist(HQ, HM): #HM is histogram of the Model Image
+    diff = cv.compareHist(HQ, HM, cv.HISTCMP_INTERSECT)
+    sum = 0
+    for i in HM:
+        sum = sum + i
+    return diff / sum
+
+# diff = compareHist(v1, v2)
+# print(diff)
+# # sum = 0
+# # for i in v2:
+# #     sum = sum + i
+# # print('sum', sum)
+# # diff = cv.compareHist(v1, v2, cv.HISTCMP_INTERSECT)
+# # print(diff / sum)
--- index.py
+++ index.py
@@ -7,11 +7,20 @@ from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFil
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 
+import cv2 as cv
+import numpy as np
+from DB import Database
+import os
+from featureExtraction import FeatureExtraction
+
 MainUI, _ = loadUiType('design.ui')
 
+DB = Database()
+featureExtraction = FeatureExtraction()
 
 class Main(QMainWindow, MainUI):
     def __init__(self, parent=None):
+        self.image = []
         super(Main, self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
@@ -19,13 +28,83 @@ class Main(QMainWindow, MainUI):
         self.comboBox.addItem('Mean Color Algorithm')
         self.comboBox.addItem('Histogram Algorithm')
         self.comboBox.addItem('Color Layout Algorithm')
+        self.pushButton_2.clicked.connect(self.initDB)
+        self.pushButton_3.clicked.connect(self.showResults)
+
+    def compareHist(self, HQ, HM):  # HM is histogram of the Model Image
+        diff = cv.compareHist(HQ, HM, cv.HISTCMP_INTERSECT)
+        sum = 0
+        for i in HM:
+            sum = sum + i
+        return diff / sum
+
+    def showResults(self):
+        selector = self.comboBox.currentText()
+        if(selector == 'Mean Color Algorithm'):
+            print(selector)
+        elif(selector == 'Histogram Algorithm'):
+            queryHist = featureExtraction.get_histogram(self.image)
+            results = DB.histogram_find()
+            matchedPaths = []
+            for result in results:
+                modelHist = np.float32(result['hist'])
+                compare = self.compareHist(queryHist, modelHist)
+                if compare > 0.5:
+                    matchedPaths.append(result['path'])
+                    print(result['path'])
+            print('matched')
+            print(matchedPaths)
+            for path in matchedPaths:
+                match = cv.imread('img/' + path)
+                cv.imshow('res', match)
+                cv.waitKey(0)
+            cv.waitKey(0)
+        elif(selector == 'Color Layout Algorithm'):
+            matchedPaths = []
+            queryHist = featureExtraction.get_color_layout(self.image) #List of 4 Lists
+            results = DB.colorLayout_find()
+            sum = 0
+            for result in results:
+                for i in range(len(queryHist)):
+                    modelHist = np.float32(result['colorLayout'][i])
+                    sum = sum + self.compareHist(queryHist[i], modelHist)
+                avg = sum / 4
+                if avg > 0.5:
+                    matchedPaths.append(avg)
+            for path in matchedPaths:
+                match = cv.imread('img/' + path)
+                cv.imshow('res', match)
+                cv.waitKey(0)
+            cv.waitKey(0)
 
     def load_query_image(self):
         img = QFileDialog.getOpenFileName(self, 'Open File')
         self.label.setPixmap(QtGui.QPixmap(img[0]))
+        self.image = cv.imread(img[0])
 
-
-
+    def initDB(self):
+        path = "C:/Users/Mohamed Ramadan/PycharmProjects/multimedia/Github/img"  # Add images dataset full path
+        DB.delete_all()
+        path, dirs, images = next(os.walk(path))
+        for imgPath in images:
+            record = {
+                "path": imgPath,
+                "features": [],
+            }
+            img = cv.imread('img/' + imgPath)  # Read the image
+            # 1- Mean Color Algorithm
+            meanColor = featureExtraction.get_mean(img)
+            record["features"].extend(meanColor)
+            # 2- calculate the histogram
+            histogram = featureExtraction.get_histogram(img)
+            record["hist"] = histogram.tolist()
+            # 3- Color layout Algorithm using histogram similarity
+            colorLayout = featureExtraction.get_color_layout(img)
+            colorLayoutList = []
+            for quarter in colorLayout:
+                colorLayoutList.append(quarter.tolist())
+            record["colorLayout"] = colorLayoutList
+            DB.insert(record)
 
 def main():
     app = QApplication(sys.argv)
